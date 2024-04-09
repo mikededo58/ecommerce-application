fa15995ef7c1af1b9f688103bc8f26cb91b6b329
mikededo58 <159196932+mikededo58@users.noreply.github.com>
2024-04-09 16:54:29 -0600

    updated models folder with model strutre

--- models/index.js
+++ models/index.js
@@ -1,11 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Category,
+    unique: false,
+  },
+  as: "product_categories",
+});
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: "ProductTag",
+    unique: false,
+  },
+  as: "product_tag",
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: "ProductTag",
+    unique: false,
+  },
+  as: "tag_product",
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
